85e2a057551aff8761ce3cb094be2a9f96433fcf
Fabio <fabioalbertocataldo@gmail.com>
2025-06-07 17:26:03 -0300

    More implementation in Cart, checkout, order id pages

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,102 @@
-export default function CheckoutPage() {
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2]
+]
+
+export default function() {
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title='Verificar orden' />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* carrito */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar elementos</span>
+            <Link href="/cart" className="underline mb-5">
+              Editar carrito
+            </Link>
+
+
+            {/* items */}
+            {
+              productsInCart.map(product => (
+                <div key={product.slug} className="flex mb-5">
+                  <Image
+                    style={{
+                      width: '100px',
+                      height: '100px'
+                    }}
+                    src={`/products/${product.images[0]}`}
+                    width={100}
+                    height={100}
+                    alt={product.title}
+                    className="mr-5 rounded"
+                    />
+
+                  <div>
+                    <p>{product.title}</p>
+                    <p>${product.price} x 3</p>
+                    <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+
+          {/* checkout resumen de orden*/}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p>Fernando Herrera</p>
+              <p>Av. siempre viva 123</p>
+              <p>Col. Centro</p>
+              <p>Alcaldía Cuahatemoc</p>
+              <p>Ciudad de México</p>
+              <p>CP 1233</p>
+              <p>123.123.123</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10"/>
+
+            <h2 className="text-2xl mb-2">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-right text-2xl">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <p className="mb-5">
+                <span className="text-xs">
+                  Al hacer click en "Colocar orden", acpetas nuestros <a href="#" className="underline">términos y condiciones</a> y <a href="#" className="underline">política de privacidad</a>
+                </span>
+              </p>
+
+              <Link 
+                className="flex btn-primary justify-center"
+                href={"/orders/123"}>
+                Colocar orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,7 +1,128 @@
-export default function OrderPage() {
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+import { IoCardOutline } from "react-icons/io5";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2]
+];
+
+interface Props {
+  params: {
+    id: string;
+  }
+}
+
+export default function ({ params }: Props) {
+  const { id } = params;
+
+  //validar
+  //redirect('//)
+
   return (
-    <div>
-      <h1>Order #abc</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title={`Orden #${id}`} />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+
+          {/* carrito */}
+          <div className="flex flex-col mt-5">
+            <div className={
+              clsx(
+                "flex items-center rounded-lg py2 px-3.5 text-xs font-bold text-white mb-5",
+                {
+                  'bg-red-500': false,
+                  'bg-green-700': true,
+                }
+              )
+            }>
+              <IoCardOutline size={30}></IoCardOutline>
+              {/* <span className="mx-2">Pendiente de pago</span> */}
+              <span className="mx-2">Pagada</span>
+            </div>
+
+
+            {/* items */}
+            {
+              productsInCart.map(product => (
+                <div key={product.slug} className="flex mb-5">
+                  <Image
+                    style={{
+                      width: '100px',
+                      height: '100px'
+                    }}
+                    src={`/products/${product.images[0]}`}
+                    width={100}
+                    height={100}
+                    alt={product.title}
+                    className="mr-5 rounded"
+                  />
+
+                  <div>
+                    <p>{product.title}</p>
+                    <p>${product.price} x 3</p>
+                    <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+
+          {/* checkout resumen de orden*/}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p>Fernando Herrera</p>
+              <p>Av. siempre viva 123</p>
+              <p>Col. Centro</p>
+              <p>Alcaldía Cuahatemoc</p>
+              <p>Ciudad de México</p>
+              <p>CP 1233</p>
+              <p>123.123.123</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+
+            <h2 className="text-2xl mb-2">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-right text-2xl">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <div className={
+                clsx(
+                  "flex items-center rounded-lg py2 px-3.5 text-xs font-bold text-white mb-5",
+                  {
+                    'bg-red-500': false,
+                    'bg-green-700': true,
+                  }
+                )
+              }>
+                <IoCardOutline size={30}></IoCardOutline>
+                {/* <span className="mx-2">Pendiente de pago</span> */}
+                <span className="mx-2">Pagada</span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
