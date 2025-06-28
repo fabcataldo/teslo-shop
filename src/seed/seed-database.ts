600b3f3fd1983d54b5da663059df381effd8c0ac
Fabio <fabioalbertocataldo@gmail.com>
2025-06-28 12:34:43 -0300

    Upload products to seed logic added in seed-database. Categories data changed in seed.ts

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,13 +1,42 @@
 import { prisma } from '../lib/prisma';
+import { initialData } from './seed';
 
 
 async function main() {
+
+    //1. Borrar registros previos
     await Promise.all([
         prisma.productImage.deleteMany(),
         prisma.product.deleteMany(),
         prisma.category.deleteMany()
     ]);
 
+    const {categories, products} = initialData;
+
+    //categorias
+    const categoriesData = categories.map(category => ({
+        name: category
+    }));
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+    const categoriesDB = await prisma.category.findMany();
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+        map[category.name.toLowerCase()] = category.id;
+        return map;
+    }, {} as Record<string, string>);
+
+     products.forEach(async (product) => {
+        const { type, images, ...rest } = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type]
+            }
+        });
+     });
 
     console.log('Seed ejecutado correctamente');
 }
