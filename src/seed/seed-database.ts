186e789b218f8e4ff5d6ee8f2f035ab2a56f3961
Fabio <fabioalbertocataldo@gmail.com>
2025-07-05 11:54:13 -0300

    Seed-database.ts script more logic added

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,10 +1,12 @@
+import { Size } from '@prisma/client';
 import { prisma } from '../lib/prisma';
 import { initialData } from './seed';
-
 
 async function main() {
 
     //1. Borrar registros previos
+    // si estoy trabajando solo creando la bd para dev, comento el
+    // await Promise.all([.... y pongo await a cada una de las 3 instrucciones
     await Promise.all([
         prisma.productImage.deleteMany(),
         prisma.product.deleteMany(),
@@ -35,6 +37,15 @@ async function main() {
                 ...rest,
                 categoryId: categoriesMap[type]
             }
+        });
+
+        const imagesData = images.map(image => ({
+            url: image,
+            productId: dbProduct.id
+        }));
+
+        await prisma.productImage.createMany({
+            data: imagesData
         });
      });
 
