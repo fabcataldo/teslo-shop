e76f864deb20484917cc476f785db895872f5ce8
Fabio <fabioalbertocataldo@gmail.com>
2025-10-01 17:21:33 -0300

    chore: clean imports and console.log

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 interface Props{
     children: React.ReactNode;
 }
 
 export const Providers = ({ children }: Props) => {
-  console.log(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID)
   return (
     <PayPalScriptProvider
       options={{
